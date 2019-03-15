8c30a12fb136d30ed835c64fbcf2b34416ca0022
JamesMusyoka <jamesmu475@gmail.com>
2019-03-15 15:21:20 +0300

    Added a code

--- aww/views.py
+++ aww/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt
 
-# Create your views here.
+def index(request):
+    date = dt.date.today
+    return render(request, 'index.html', {"date": date})
+
+def profile(request):
+    date = dt.date.today
+    return render(request, {"date": date})
+
+def convert_dates(dates):
+    day_number = dt.date.weekday(dates)
+    day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    day = days[day_number]
+    return day
+
+def project(request):
+    date = dt.date.today
+    return render(request, {"date": date})
+    
