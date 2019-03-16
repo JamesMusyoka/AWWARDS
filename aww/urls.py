26e117e6f5d9e37d22519c7871a84b81a78e9a04
JamesMusyoka <jamesmu475@gmail.com>
2019-03-16 07:55:53 +0300

    added codes

--- aww/urls.py
+++ aww/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url('^today/$', views.profile,name='profile'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
+
