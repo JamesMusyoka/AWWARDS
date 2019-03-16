8ce5dbde5b43afd1b208faae7e24b7e95fa5d7ed
JamesMusyoka <jamesmu475@gmail.com>
2019-03-16 06:30:11 +0300

    registredr the classes

--- aww/admin.py
+++ aww/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project, Profile, Rate
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal =('rate')
+
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Rate)
+
