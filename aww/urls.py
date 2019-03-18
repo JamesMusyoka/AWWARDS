28f28d956a9442bee7d028b3c3675f2209fa9d65
JamesMusyoka <jamesmu475@gmail.com>
2019-03-18 17:33:51 +0300

    Added a url for signup

--- aww/urls.py
+++ aww/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import  url
 from . import views
 
 urlpatterns=[
-    # url('^$', views.signup, name='signup'),
-    url('^$',views.index,name = 'index'),
+    url('^$', views.signup, name='signup'),
+    url(r'index',views.index,name = 'index'),
     url(r'^projects/',views.project, name='projects'),
     url(r'^profile/', views.profile,name='profile'),
     url(r'^search/', views.search_results, name='search_results'),
